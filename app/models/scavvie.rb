779d8717076c8ceccc90e72d39a327787950e4b5
NickyEXE <dovern42@gmail.com>
2020-05-04 00:42:26 -0400

    now generates skills and associations

--- app/models/scavvie.rb
+++ app/models/scavvie.rb
@@ -1,8 +1,10 @@
 class Scavvie < ApplicationRecord
+    has_many :scavvie_skills
+    has_many :skills, through: :scavvie_skills
+
 
     # add software
     def self.get_scavvies_from_spreadsheet(values)
-
         values.each{|scavvie_row| self.create_by_spreadsheet_values(scavvie_row)}
     end
 
@@ -10,7 +12,9 @@ class Scavvie < ApplicationRecord
         keys = ["name", "pronouns", "email", "phone", "location", "skills", "skill_description", "software", "czar_interest", "yearbook_quote", "about"]
         hash = Hash[keys.zip(arr)]
         hash["image_url"] = self.get_an_image
-        Scavvie.create(hash.except("skills", "software")) 
+        scavvie = Scavvie.create(hash.except("skills", "software"))
+        scavvie.skills_from_string(hash["skills"])
+        scavvie
     end
 
 
@@ -19,4 +23,8 @@ class Scavvie < ApplicationRecord
         JSON.parse(response)["message"]
     end
 
+    def skills_from_string(string_of_comma_separated_skills)
+        skills_as_array = string_of_comma_separated_skills.split(", ")
+        self.skills << Skill.where(name: skills_as_array)
+    end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+    has_many :scavvie_skills
+    has_many :scavvies, through: :scavvie_skills
 end
