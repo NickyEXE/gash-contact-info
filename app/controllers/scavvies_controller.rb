0b84f8be706e7a20d37f5267a34dbac27945ac3a
NickyEXE <dovern42@gmail.com>
2020-05-04 01:15:39 -0400

    basic show and index

--- app/controllers/scavvies_controller.rb
+++ app/controllers/scavvies_controller.rb
@@ -1,2 +1,11 @@
 class ScavviesController < ApplicationController
+
+    def index
+        @scavvies = Scavvie.all.sort_by{|scavvie| scavvie.name.downcase}
+    end
+
+    def show
+        @scavvie = Scavvie.find(params[:id])
+    end
+
 end
